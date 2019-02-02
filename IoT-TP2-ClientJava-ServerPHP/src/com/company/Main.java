76764d31c9ef8f1c8768898247988a4a050ac2a0
Unknown <theoremond@gmail.com>
2019-02-02 11:54:24 +0100

    :tada: FINISH

--- IoT-TP2-ClientJava-ServerPHP/src/com/company/Main.java
+++ IoT-TP2-ClientJava-ServerPHP/src/com/company/Main.java
@@ -28,38 +28,73 @@ public class Main {
 
                 System.out.println("1 \'GET\'  -->");
                 System.out.println("2 \'POST\' -->");
-                String choixVerbeHttp = sc.nextLine();
-                switch (choixVerbeHttp) {
+                String choixVerbeHttpHello = sc.nextLine();
+                switch (choixVerbeHttpHello) {
                     case "1":
-                        get(socket, nom, prenom);
+                        getHello(socket, nom, prenom);
                         break;
                     case "2":
-                        post(socket, nom, prenom);
+                        postHello(socket, nom, prenom);
                         break;
                 }
                 break;
             case "2":
                 // INPUT VALUES A/B 
                 System.out.println("a = ");
-                int a = sc.nextInt();
+                String a = sc.nextLine();
                 System.out.println("b = ");
-                int b = sc.nextInt();
+                String b = sc.nextLine();
 
+                System.out.println("1 \'GET\'  -->");
+                System.out.println("2 \'POST\' -->");
+                String choixVerbeHttpNumbers = sc.nextLine();
+                switch (choixVerbeHttpNumbers) {
+                    case "1":
+                        getNumbers(socket, a, b);
+                        break;
+                    case "2":
+                        postNumbers(socket, a, b);
+                        break;
+                }
                 break;
         }
 
         socket.close();
     }
 
-    private static void get(Socket sock, String nom, String prenom) throws IOException {
+    private static void getHello(Socket sock, String nom, String prenom) throws IOException {
         String request = "GET /?nom=" + nom + "&prenom=" +prenom + "\r\nHost: localhost\r\n\r\n";
+        sendGetRequest(sock, request);
+    }
+
+    private static void postHello(Socket sock, String nom, String prenom) throws IOException {
+        //TODO remove headers
+        String data = URLEncoder.encode("nom", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(nom, StandardCharsets.UTF_8) + "&";
+        data += URLEncoder.encode("prenom", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(prenom, StandardCharsets.UTF_8);
+
+        sendPostRequest(sock, data);
+    }
+
+    private static  void getNumbers(Socket socket, String a, String b) throws IOException {
+        String request = "GET /?a=" + a + "&b=" + b + "\r\nHost: localhost\r\n\r\n";
+        sendGetRequest(socket, request);
+    }
+
+    private static  void postNumbers(Socket socket, String a, String b) throws IOException {
+        String data = URLEncoder.encode("a", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(a, StandardCharsets.UTF_8) + "&";
+        data += URLEncoder.encode("b", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(b, StandardCharsets.UTF_8);
+
+        sendPostRequest(socket, data);
+    }
+
+    private static void sendGetRequest(Socket socket, String request) throws IOException {
         StringBuilder content = new StringBuilder();
 
-        BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
+        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
         bos.write(request.getBytes());
         bos.flush();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String inputLine;
         while ((inputLine = reader.readLine()) != null) {
             content.append(inputLine).append("\n");
@@ -67,20 +102,8 @@ public class Main {
         System.out.println(content);
     }
 
-    private static void post(Socket sock, String nom, String prenom) throws IOException {
-        //TODO remove headers
-        String data = URLEncoder.encode("nom", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(nom, StandardCharsets.UTF_8) + "&";
-        data += URLEncoder.encode("prenom", StandardCharsets.UTF_8) + "=" + URLEncoder.encode(prenom, StandardCharsets.UTF_8);
-
-        /*String request = "POST / HTTP/1.1\r\n" +
-                "Content-Length: " + data.length() +
-                "\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\n" +
-                data;
-        BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
-        bos.write(request.getBytes());
-        bos.flush();*/
-
-        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream(), StandardCharsets.UTF_8));
+    private static void sendPostRequest(Socket socket, String data) throws IOException {
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
         wr.write("POST / HTTP/1.0\r\n");
         wr.write("Content-Length: " + data.length() + "\r\n");
         wr.write("Content-Type: application/x-www-form-urlencoded\r\n");
@@ -88,7 +111,7 @@ public class Main {
         wr.write(data);
         wr.flush();
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String line;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 
 if(!empty($_POST)) {
     if(isset($_POST['nom']) && $_POST['prenom']) {
-    
+        echo 'Bonjour, ' . $_POST['nom'] . ' ' . $_POST['prenom'] . ' !';
     } else if (isset($_POST['a']) && isset($_POST['b'])) {
-
+        $r = intval($_POST['a']) + intval($_POST['b']);
+        echo $r;
     }
 } else if (!empty($_GET)) {
     if(isset($_GET['nom']) && isset($_GET['prenom'])) {
-
+        echo 'Bonjour, ' . $_GET['nom'] . ' ' . $_GET['prenom'] . ' !';
     } else if(isset($_GET['a']) && isset($_GET['b'])) {
-    
+        $r = intval($_GET['a']) + intval($_GET['b']);
+        echo $r;
     }
 } else {
     echo 'Il a pas dit bonjour ! ';
